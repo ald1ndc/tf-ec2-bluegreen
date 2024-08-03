b38ac2affa76966acd9d42fce657cefa93d8d0f4
ald1ndc <aldindc@yahoo.com>
2024-08-03 11:51:41 +0800

    terraform blue green

--- main.tf
+++ main.tf
@@ -53,11 +53,12 @@ resource "aws_security_group" "app" {
   vpc_id      = data.aws_vpc.selected.id
 
   ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    #cidr_blocks      = ["0.0.0.0/0"]
+    #ipv6_cidr_blocks = ["::/0"]
+    security_groups = [aws_security_group.alb.id]
   }
 
   egress {
@@ -84,23 +85,23 @@ resource "aws_lb_listener" "app" {
   protocol          = "HTTP"
 
   default_action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = aws_lb_target_group.blue.arn
-    # forward {
-    #   target_group {
-    #     arn =     aws_lb_target_group.blue.arn
-    #     weight = lookup(local.traffic_dist_map[var.traffic_distribution], "blue", 100)
-    #   }
+    forward {
+      target_group {
+        arn =     aws_lb_target_group.blue.arn
+        weight = lookup(local.traffic_dist_map[var.traffic_distribution], "blue", 100)
+      }
 
-    #   target_group {
-    #     arn =    aws_lb_target_group.green.arn
-    #     weight = lookup(local.traffic_dist_map[var.traffic_distribution], "green", 0)
-    #   }
+      target_group {
+        arn =    aws_lb_target_group.green.arn
+        weight = lookup(local.traffic_dist_map[var.traffic_distribution], "green", 0)
+      }
 
-    #   stickiness {
-    #     enabled  = false
-    #     duration = 1
-    #   }
-    # }
+      stickiness {
+        enabled  = false
+        duration = 1
+      }
+    }
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 variable "name" {
   description = "Unique identifier for the user"
   type        = string
-  default     = ""          # to replace
+  default     = "aldindc" # to replace
 }
 
 variable "vpc_id" {
   description = "Virtural Private Cloud ID"
   type        = string
-  default     = ""          # to replace
+  default     = "vpc-0411632e16afd09a2" # to replace
 }
 
 variable "enable_blue_env" {
@@ -16,38 +16,38 @@ variable "enable_blue_env" {
   default     = true
 }
 
-# variable "enable_green_env" {
-#   description = "Enable green environment"
-#   type        = bool
-#   default     = false
-# }
+variable "enable_green_env" {
+  description = "Enable green environment"
+  type        = bool
+  default     = false
+}
 
-# variable "traffic_distribution" {
-#   description = "Levels of traffic distribution"
-#   type        = string
-# }
+variable "traffic_distribution" {
+  description = "Levels of traffic distribution"
+  type        = string
+}
 
-# locals {
-#   traffic_dist_map = {
-#     blue = {
-#       blue  = 100
-#       green = 0
-#     }
-#     blue-90 = {
-#       blue  = 90
-#       green = 10
-#     }
-#     split = {
-#       blue  = 50
-#       green = 50
-#     }
-#     green-90 = {
-#       blue  = 10
-#       green = 90
-#     }
-#     green = {
-#       blue  = 0
-#       green = 100
-#     }
-#   }
-# }
+locals {
+  traffic_dist_map = {
+    blue = {
+      blue  = 100
+      green = 0
+    }
+    blue-90 = {
+      blue  = 90
+      green = 10
+    }
+    split = {
+      blue  = 50
+      green = 50
+    }
+    green-90 = {
+      blue  = 10
+      green = 90
+    }
+    green = {
+      blue  = 0
+      green = 100
+    }
+  }
+}
