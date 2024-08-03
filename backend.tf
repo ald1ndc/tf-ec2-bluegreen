2ea5485f27187bc18f94614867e8d647784728fe
ald1ndc <aldindc@yahoo.com>
2024-08-03 12:31:53 +0800

    terraform blue green v6

--- backend.tf
+++ backend.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = ">= 1.0"
   backend "s3" {
     bucket = "sctp-ce6-tfstate"              # Terraform State bucket name
     key    = "aldindc-ec2-bluegreen.tfstate" # Name of your tfstate file
